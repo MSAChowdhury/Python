a9fd180028da264e34ccd9eb6d9fa1200190249e
MSAChowdhury <samselarifinchowdhury@gmail.com>
2020-03-25 02:11:30 +0600

    write the value of the expression

---
+++ Prb5.py
@@ -0,0 +1,9 @@
+width = 17
+height = 12.0
+
+res1 = width//2
+res2 = width/2.0
+res3 = height/3
+res4 = 1 + 2 * 5
+
+print("Result1 = ",res1,"\nResult2 = ",res2,"\nResult3 = ",res3,"\nResult4 = ",res4)
