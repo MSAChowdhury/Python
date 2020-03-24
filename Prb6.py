a3b782d1c53a417e9acdcc125654bfdd1cef97db
MSAChowdhury <samselarifinchowdhury@gmail.com>
2020-03-25 03:01:16 +0600

    Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature

---
+++ Prb6.py
@@ -0,0 +1,10 @@
+n = input("Press '1' to find Temperate in Celsius\n//\nPress '2' to find Temperate in Farenheit\n: ")
+if int(n) == 1:
+    farenheit = input("Enter the temperature in Farenheit: ")
+    celsius = float(farenheit) - 32 * (9/5)
+    print("The temperature in Celsius is: %.2f" % round(celsius,2))
+else:
+    celsius = input("Enter the temperature in Celsius: ")
+    farenheit = float(celsius) * (5/9) + 32
+    print("The temperature in Farenheit is: {0:.2f}".format(farenheit))
+# to have precision after decimal point in python, there are two types ("%.2f" % round(celsius,2)) or "{0:.2f}".format(farenheit) or "{0:.2f}".format(round(farenheit,2))
